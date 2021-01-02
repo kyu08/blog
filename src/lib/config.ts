45ebf541ca853952eae62cd82acc72f683712781
kyu08 <kyu08@users.noreply.github.com>
2021-01-02 23:22:38 +0900

    delete ogp

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,16 +2,17 @@ export const BLOG_TITLE = 'blog.kyu08.com'
 export const BLOG_URL = 'https://blog.kyu08.com/'
 export const DEFAULT_OGP_IMAGE =
   'https://s3-ap-northeast-1.amazonaws.com/blog.kyu08.com/defaultOgp.png'
-// export const DEFAULT_OGP_IMAGE = '../../public/defaultOgp.png'
 export const DEFAULT_DESCRIPTION = 'kyu08のブログです'
 export const FAVICON = 'public/favicon.ico'
 
-type modeType = 'dev' | 'prod'
 type postsDirectoryType = 'postsForDev' | 'posts'
-const MODE: modeType = 'dev'
-const postsDirectoryMap: Map<modeType, postsDirectoryType> = new Map([
-  ['dev', 'postsForDev'],
-  ['prod', 'posts'],
-])
+
+// todo MODE を切り替えればすべて切り替わるようにしたい
+// type modeType = 'dev' | 'prod'
+// const MODE: modeType = 'dev'
+// const postsDirectoryMap: Map<modeType, postsDirectoryType> = new Map([
+//   ['dev', 'postsForDev'],
+//   ['prod', 'posts'],
+// ])
+
 export const postsDirectory: postsDirectoryType = 'posts'
-// export const postsDirectory: postsDirectoryType = postsDirectoryMap.get(MODE)
