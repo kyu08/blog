4c311880a345661912650efb0a022bb05bd586b0
kyu08 <kyu08@users.noreply.github.com>
2021-01-03 02:40:24 +0900

    modify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import styles from './Footer.module.css'
 
 export default function Footer() {
-  const year = 2020
+  const year = new Date().getFullYear()
   return (
     <>
       <p className={styles.footer}>©︎{year} kyu08, All Rights Reserved</p>
+      <p className={styles.footer}>This site uses Google Analytics.</p>
     </>
   )
 }
