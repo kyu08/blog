f4587d4c0499ba864d40c8a9adf8937ceffe34e4
kyu08 <kyu08@users.noreply.github.com>
2021-01-02 21:08:13 +0900

    add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
-import { AppProps } from 'next/app'
-
 import '../../styles/global.css'
 import 'highlight.js/styles/ocean.css'
+
+import { AppProps } from 'next/app'
 import HeaderComponent from '@/components/HeaderComponent'
 import Footer from '@/components/Footer'
+import * as gtag from '@/lib/gtag'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <HeaderComponent />
