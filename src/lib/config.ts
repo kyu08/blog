966bbe340f9bccbfe52d55842a7299fce44421d8
kyu08 <kyu08@users.noreply.github.com>
2021-01-02 23:17:51 +0900

    画像をS3のアドレスに

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,8 @@
 export const BLOG_TITLE = 'blog.kyu08.com'
 export const BLOG_URL = 'https://blog.kyu08.com/'
-export const DEFAULT_OGP_IMAGE = '../../public/defaultOgp.png'
+export const DEFAULT_OGP_IMAGE =
+  'https://s3-ap-northeast-1.amazonaws.com/blog.kyu08.com/defaultOgp.png'
+// export const DEFAULT_OGP_IMAGE = '../../public/defaultOgp.png'
 export const DEFAULT_DESCRIPTION = 'kyu08のブログです'
 export const FAVICON = 'public/favicon.ico'
 
