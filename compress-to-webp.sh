ba1c5d043f454262c4afc6099b4b921c8910ac56
Tatsuya Kyushima <49891479+kyu08@users.noreply.github.com>
2025-06-14 21:46:30 +0900

    chore: 画像変換時に向きを保持する (#221)

--- compress-to-webp.sh
+++ compress-to-webp.sh
@@ -8,6 +8,6 @@ TARGET=$(find ./content/posts -type d | fzf --prompt="Select a directory: " --pr
 # webpに変換
 for file in $TARGET/*.{jpg,jpeg,png,JPG,JPEG,PNG};
   do if [[ "$file" != *"*.png" && "$file" != *"*.jpg" && "$file" != *"*.jpeg" && "$file" != *"*.PNG" && "$file" != *"*.JPG" && "$file" != *"*.JPEG" ]]; then
-    cwebp "$file" -o "${file%.*}.webp"
+    cwebp -metadata all "$file" -o "${file%.*}.webp"
   fi;
 done
